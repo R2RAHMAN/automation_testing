f6765c0a6a701ef00d072b4c1794e7e088a35c30
R2RAHMAN <169032482+R2RAHMAN@users.noreply.github.com>
2024-09-25 09:49:33 +0600

    Add files via upload

---
+++ src/test/java/features/searchCity.feature
@@ -0,0 +1,14 @@
+Feature: Search City
+  I am a user
+  I want to search
+  To see the weather of the city
+
+
+  Scenario: Search for New York
+      Given The main page is opened
+      And   I click on search field
+      When  I input "New York" in the search field
+      Then  the search results list is displayed
+      When  I click on the first search result
+      Then  the city weather page header contains city name "New York"
+      Then  I print "hello world"
---
+++ src/test/java/stepdefinitions/CityWeatherPageSteps.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+import pages.CityWeatherPage;
+
+import static io.qameta.allure.Allure.step;
+
+public class CityWeatherPageSteps {
+    CityWeatherPage cityWeatherPage = new CityWeatherPage();
+
+    @Then("the city weather page header contains city name {string}")
+    public void theCityWeatherPageHeaderContainsCityName(String cityName) {
+        step("Check that city weather page header contains city name");
+        String header = cityWeatherPage.getCityName();
+        Assert.assertTrue(header.contains(cityName), "City name is not present in the header");
+    }
+
+    @Then("I print {string}")
+    public void iPrint(String arg0) {
+        step("Print " + arg0);
+        System.out.println(arg0);
+    }
+}
